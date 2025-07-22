a95c0ce97c74a2a1467de22950db5b2bab2e0c2b
naldobr-dev <naldobr.dev@gmail.com>
2025-07-21 22:41:57 -0300

    Corrigidos alguns erros na chamada da API em (frontend)LoginPage.tsx

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -22,7 +22,7 @@ export default function LoginPage({ onLoginSuccess }: Props) {
         setError('');
 
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
                 username,
                 password,
             });
